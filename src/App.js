6da6b74474a92d68fe65c25fb4542bcc9b893fe3
k-tsurumaki <92656989+k-tsurumaki@users.noreply.github.com>
2023-07-10 09:03:46 +0900

    Navbarを追加

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Home from "./components/Home/Home";
 import CreatePost from "./components/CreatePost/CreatePost";
 import Login from "./components/Login/Login";
 import Logout from "./components/Logout/Logout";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/createpost" element={<CreatePost />}></Route>
